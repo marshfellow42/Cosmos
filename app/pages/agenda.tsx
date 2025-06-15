409bcc14f77c4c90709422fac08adf106fea0f7b
LucasCostaMrq <lucascostamarques816@gmail.com>
2025-06-15 20:13:15 -0300

    implementação da navegação do projeto

---
+++ app/pages/agenda.tsx
@@ -0,0 +1,9 @@
+import { View, Text, StyleSheet, Image, TouchableOpacity, Modal } from "react-native"
+
+export function Agenda(){
+    return(
+        <View>
+            <Text>Im agenda</Text>
+        </View>
+    )
+}
---
+++ app/pages/groups.tsx
@@ -0,0 +1,9 @@
+import { View, Text, StyleSheet, Image, TouchableOpacity, Modal } from "react-native"
+
+export function Groups(){
+    return(
+        <View>
+            <Text>Im groups</Text>
+        </View>
+    )
+}
